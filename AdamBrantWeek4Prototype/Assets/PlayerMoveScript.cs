97e611bc6823b98b32184628c25694957d498501
Tranzorro <shiekadam@gmail.com>
2018-04-24 21:28:36 -0400

    fuck my life
    initial push

---
+++ AdamBrantWeek4Prototype/Assets/PlayerMoveScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+    public float moveSpeed;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+}
---
+++ AdamBrantWeek4Prototype/Assets/SpawnStuff.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStuff : MonoBehaviour {
+
+    public GameObject ObsticlePrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(SpawnObsticle());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    IEnumerator SpawnObsticle()
+    {
+        transform.position = new Vector2(Random.Range(0, 600));
+        Instantiate(ObsticlePrefab, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(SpawnObsticle());
+    }
+
+    
+}
+
+
+
+/*
+public class SpawnObject : MonoBehaviour
+{
+
+    public GameObject WallPrefab;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        StartCoroutine(SpawnWall());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    IEnumerator SpawnWall()
+    {
+        Instantiate(WallPrefab, transform.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine(SpawnWall());
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+
+
+
+
+}
+*/
